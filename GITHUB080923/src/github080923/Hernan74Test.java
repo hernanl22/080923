310e85858e9de444d73dd1db7c706205146a6fe3
Herny74 <hernanfermion@gmail.com>
2023-08-09 20:01:18 -0300

    Hernan74Test
    Chequeo de variables nuevas.

---
+++ GITHUB080923/src/github080923/Hernan74Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github080923;
+
+/**
+ *
+ * @author herna
+ */
+public class Hernan74Test {
+    public int edad;
+    public String nombre;
+}
